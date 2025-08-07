045d42e45f28114896ec59b7860edea7d329be1c
mishka <mishkakharlov1@gmail.com>
2025-08-07 11:26:20 -0700

    stream

--- eventSourceStream.py
+++ eventSourceStream.py
@@ -7,10 +7,19 @@ with EventSource(url) as stream:
         if event.type == 'message':
             try:
                 change = json.loads(event.data)
+                
             except ValueError:
                 pass
             else:
                 # discard canary events
                 if change['meta']['domain'] == 'canary':
                     continue            
-                print('{user} edited {title}'.format(**change))
+                '''
+
+                ['$schema', 'meta', 'type', 'namespace', 'title', 'title_url', 'comment', 'timestamp', 'user', 'bot', 'log_id', 
+                           'log_type', 'log_action', 'log_params', 'log_action_comment', 'server_url', 'server_name', 'server_script_path',
+                           'wiki', 'parsedcomment']
+                '''
+                # print('{user} edited {title}'.format(**change))
+                print(change['title'])
+
