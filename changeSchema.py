17345c4916881047bf68ba39a67a2df48b2d2fb6
mishka <mishkakharlov1@gmail.com>
2025-08-08 08:41:40 -0700

    stream done

---
+++ changeSchema.py
@@ -0,0 +1,5 @@
+'''
+    ['$schema', 'meta', 'type', 'namespace', 'title', 'title_url', 'comment', 'timestamp', 'user', 'bot', 'log_id', 
+        'log_type', 'log_action', 'log_params', 'log_action_comment', 'server_url', 'server_name', 'server_script_path',
+        'wiki', 'parsedcomment']
+''' 
--- eventSourceStream.py
+++ eventSourceStream.py
@@ -1,25 +1,33 @@
-import json
+import json, os, boto3, gzip
 from requests_sse import EventSource
+import datetime as dt
+
+bucket = "wikichangesbucket"
+s3 = boto3.client("s3")
 
 url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+
+def object_key(ts: dt.datetime, chunk_id: int) -> str:
+    return f"date={ts:%Y-%m-%d}/hour={ts:%H}/rc_{ts:%Y%m%dT%H%M%S}_{chunk_id:04}.json.gz"
+
+CHUNK_ROWS, chunk, chunk_id = 20_000, [], 0
 with EventSource(url) as stream:
     for event in stream:
         if event.type == 'message':
             try:
                 change = json.loads(event.data)
-                
             except ValueError:
                 pass
             else:
                 # discard canary events
                 if change['meta']['domain'] == 'canary':
                     continue            
-                '''
-
-                ['$schema', 'meta', 'type', 'namespace', 'title', 'title_url', 'comment', 'timestamp', 'user', 'bot', 'log_id', 
-                           'log_type', 'log_action', 'log_params', 'log_action_comment', 'server_url', 'server_name', 'server_script_path',
-                           'wiki', 'parsedcomment']
-                '''
-                # print('{user} edited {title}'.format(**change))
-                print(change['title'])
-
+                
+                #start processing through
+                chunk.append(json.dumps(change, ensure_ascii=False))
+                if len(chunk) < CHUNK_ROWS:
+                    curr_time = dt.datetime.now
+                    object_key = object_key(curr_time, chunk_id)
+                    body = gzip.compress("\n".join(chunk).encode())
+                    s3.put_object(Bucket = bucket, Key = object_key, Body = body)
+                    chunk, chunk_id = [], 0
