9ae2b6f0b75edbbf18ac51211fa0efc9d11d55b8
mishka <mishkakharlov1@gmail.com>
2025-08-09 12:43:04 -0700

    whatever at this point

--- clean.py
+++ clean.py
@@ -6,10 +6,10 @@ spark = SparkSession.builder\
         .getOrCreate()
         
         
-#how to automate this?
+#todo automate this
 raw_df = spark.read.json("/Users/mish/Documents/Code/Projects/wikiEmbeddings/data/20000.json")
 
-
+#python below
 clean_df = raw_df.select(
     col("meta.dt").alias("timestamp"),
     "user",
@@ -20,10 +20,37 @@ clean_df = raw_df.select(
     "comment",
     col("meta.uri").alias("uri"),
     col("meta.domain").alias("domain")
-)
-clean_df.head(5)
-# print(clean_df.head(5))
+).filter(col("bot") == False)
+
+
+#wait on this
 # clean_df.write \
 #     .mode("overwrite") \
 #     .partitionBy("timestamp") \
 #     .parquet("s3://wikichangesbucket/processed/")
+
+
+
+
+
+
+
+# #sql
+# cleanql = spark.sql(
+#     """
+#     SELECT 
+#     CAST(meta.dt AS TIMESTAMP) AS timestamp,
+#     user,
+#     type,
+#     comment,
+#     wiki,
+#     meta.uri AS uri,
+#     meta.domain AS domain
+#     FROM raw_df
+#     WHERE bot == false                
+                    
+                    
+#     """    
+# )
+
+
